c4816c3a0f24d0b61cbee9524fdeae43b36360ca
Anna Maria Eilertsen <anna.eilertsen@uib.no>
2019-07-23 11:12:00 -0700

    before move

---
+++ src/org/main/A.java
@@ -0,0 +1,10 @@
+package org.main;
+
+public class A {
+    B b = new B();
+
+    void foo(){
+        b.m();
+        b.n();
+    }
+}
---
+++ src/org/main/B.java
@@ -0,0 +1,15 @@
+package org.main;
+
+public class B {
+    private String myVar = "myVar";
+
+    void m(){
+        myVar = "B.m";
+        System.out.println(myVar);
+    }
+
+    void n(){
+        myVar = "B.n";
+        System.out.println(myVar);
+    }
+}
